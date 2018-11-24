d256cedf86f9093cf5c50fbe646912317d627795
subajat1 <bayumunajat@outlook.com>
2018-11-24 20:55:21 +0800

    register post to admin page

--- foxblog_project/blog/admin.py
+++ foxblog_project/blog/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    fields = ('author', 'title', 'content', 'date_posted',)
+    list_display = ('title', 'author', 'date_posted')
