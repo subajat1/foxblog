0efaee167829a7f5086facab93773fc696627748
subajat1 <bayumunajat@outlook.com>
2018-11-24 23:38:20 +0800

    improve resize profile pics

--- foxblog_project/users/models.py
+++ foxblog_project/users/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,3 +11,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return '{} Profile'.format(self.user.username)
+
+    def save(self):
+        super(Profile, self).save()
+
+        img = Image.open(self.image.path)
+        
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
