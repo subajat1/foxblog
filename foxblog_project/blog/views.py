b179da8a2ad2d6ca4c557dc5a4435fd43b4f3961
subajat1 <bayumunajat@outlook.com>
2018-11-24 20:56:33 +0800

    improve post viewer display

--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+from .models import Post
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'title': 'Fox Blog',
+        'posts': Post.objects.all(),
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
