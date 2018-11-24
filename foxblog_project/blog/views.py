495d2c7d263417da6afa7925b527bf38c82275a0
subajat1 <bayumunajat@outlook.com>
2018-11-24 19:35:29 +0800

    develop basic template and migrate for admin

--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def dev(request):
+    return render(request, 'blog/dev.html')
