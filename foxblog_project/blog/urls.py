d9d67b012c0dac007578c95a8e7fe2c6a033816e
subajat1 <bayumunajat@outlook.com>
2018-11-24 15:15:57 +0800

    improve web push notification

--- foxblog_project/blog/urls.py
+++ foxblog_project/blog/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('notif/', views.notif, name='blog-notif'),
+    path('subscribe/', views.subscribe_notification, name='blog-subscribe-notif'),
+    path('notif_users/', views.notif_users_specific_group, name='blog-notif-users'),
+    path('notif_group/', views.notif_group, name='blog-notif-group'),
+
 ]
--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -13,23 +13,62 @@ def home(request):
 
 def about(request):
     context = {'title': 'About!'}
-    user = User.objects.get(pk=1)
 
     return render(request, 'blog/about.html', context)
 
-def notif(request):
-    context = {
-        'title': 'About!',
-        'webpush': {'group': 'admin'}
-    }
-    user = User.objects.get(pk=1)
-    print(user)
-    payload = {'head': 'Simple welcome notification', 'body': 'Hello notification using django web-push!!'}
-    try:
-        ''' send_user_notification still not working '''
-        # send_user_notification(user=user, payload=payload, ttl=1000)
-        send_group_notification(group_name='admin', payload=payload, ttl=1000)
-    except:
-        print('push notif failed')
+def subscribe_notification(request):
 
-    return render(request, 'blog/notif.html', context)
+    context = {}
+    if request.user.is_authenticated:
+        group = request.user.groups.all()[0]
+        context = {'webpush': {'group': group}}
+     
+    return render(request, 'blog/subscribe.html', context)
+
+def notif_users_specific_group(request):
+    group = 'admin'
+    payload_icon = 'https://i.imgur.com/dRDxiCQ.png'
+    payload_url = 'http://google.com'
+    
+    if request.user.is_authenticated:
+        print(request.user)
+
+        payload = {"head": "Welcome!", "body": "Hello World"}
+        send_user_notification(user=request.user, payload=payload, ttl=1000)
+
+    users = User.objects.filter(groups__name=group)
+    print(group)
+    if len(users) > 0:
+        print(users)
+        # for user in users:
+        #     payload = {"head": "Welcome!", "body": "Hello World"}
+        #     send_user_notification(user=user, payload=payload, ttl=1000)
+
+        # try:
+        #     for user in users:
+        #         print(user)
+        #         payload = { 'head': '[{}] New update!'.format(group), 
+        #                     'body': 'Hi {}, you have a new update ...'.format(str(user)),
+        #                     'icon': payload_icon,
+        #                     'url': payload_url}
+        #         payload = {"head": "Welcome!", "body": "Hello World"}
+        #         send_user_notification(user=user, payload=payload, ttl=1000)
+        # except IndexError:
+        #     print('send_user_notification to group: {} is failed.'.format(group))
+    else:
+        print('no user for group: '.format(group))
+
+    # context = {'users': users, 'group': group}
+    context = {}
+    return render(request, 'blog/notif_users.html', context)
+
+def notif_group(request):
+    group = 'admin'
+    
+    payload = {"head": "Welcome!", "body": "Hello World"}
+    if request.user.is_authenticated:
+        send_group_notification(group_name='admin', payload=payload, ttl=1000)
+    # send_group_notification(group_name=group, payload=payload, ttl=1000)
+
+    context = {'users': users, 'group': group}
+    return render(request, 'blog/notif_group.html')
