d27a6eddcf0ff42c88c812fa07b02cfe667e8c24
subajat1 <bayumunajat@outlook.com>
2018-11-24 22:56:40 +0800

    setup profile picture media

--- foxblog_project/foxblog_project/urls.py
+++ foxblog_project/foxblog_project/urls.py
@@ -27,3 +27,9 @@ urlpatterns = [
     url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
