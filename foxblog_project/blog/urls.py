6330eb8cf021e742134ce780dafb50e7e2edee78
subajat1 <bayumunajat@outlook.com>
2018-11-25 10:21:38 +0800

    develop Post DetailView

--- foxblog_project/blog/urls.py
+++ foxblog_project/blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='blog-home'),
+    url(r'^post/(?P<pk>\d+)', PostDetailView.as_view(), name='post-detail'),
     url(r'^dev/$', views.dev, name='blog-dev'),
     url(r'^about/', views.about, name='blog-about'),
 ]
--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -17,6 +17,10 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
 def about(request):
     return render(request, 'blog/about.html')
