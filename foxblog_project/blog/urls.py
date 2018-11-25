7d729b3008828319e8793154adeefbbf3244ed21
subajat1 <bayumunajat@outlook.com>
2018-11-25 10:54:01 +0800

    update view for post

--- foxblog_project/blog/urls.py
+++ foxblog_project/blog/urls.py
@@ -3,13 +3,15 @@ from . import views
 from .views import (
     PostListView, 
     PostDetailView, 
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='blog-home'),
-    url(r'^post/(?P<pk>\d+)', PostDetailView.as_view(), name='post-detail'),
     url(r'^post/new/', PostCreateView.as_view(), name='post-create'),
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
+    url(r'^post/(?P<pk>\d+)/update/$', PostUpdateView.as_view(), name='post-update'),
     url(r'^dev/$', views.dev, name='blog-dev'),
     url(r'^about/', views.about, name='blog-about'),
 ]
--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView
+    CreateView,
+    UpdateView
 )
 
 from .models import Post
@@ -34,6 +35,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(PostCreateView, self).form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostUpdateView, self).form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html')
