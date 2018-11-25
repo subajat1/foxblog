31315daaae55a8fce334b9153382ef635143108f
subajat1 <bayumunajat@outlook.com>
2018-11-25 10:06:33 +0800

    develop ListView for Post

--- foxblog_project/blog/urls.py
+++ foxblog_project/blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    url(r'^$', views.home, name='blog-home'),
+    url(r'^$', PostListView.as_view(), name='blog-home'),
     url(r'^dev/$', views.dev, name='blog-dev'),
     url(r'^about/', views.about, name='blog-about'),
 ]
--- foxblog_project/blog/views.py
+++ foxblog_project/blog/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Post
 
@@ -10,6 +11,12 @@ def home(request):
         'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html')
