c84c55b4a621347deef41dba1d02a63d94a355ef
subajat1 <bayumunajat@outlook.com>
2018-11-25 13:38:30 +0800

    improvement pagination foreach users

--- foxblog_project/blog/urls.py
+++ foxblog_project/blog/urls.py
@@ -5,11 +5,13 @@ from .views import (
     PostDetailView, 
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='blog-home'),
+    url(r'^user/(?P<username>\S+)/$', UserPostListView.as_view(), name='user-posts'),
     url(r'^post/new/', PostCreateView.as_view(), name='post-create'),
     url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
     url(r'^post/(?P<pk>\d+)/update/$', PostUpdateView.as_view(), name='post-update'),
